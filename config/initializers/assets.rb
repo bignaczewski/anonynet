f8095254e8dcd1357e73de5e8216c71d59e40909
Bartosz Ignaczewski <bartosz.p.ignaczewski@gmail.com>
2015-07-21 00:38:54 +0200

    assets production fix 2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += [/.*\.js/, /.*\.css/]
+Rails.application.config.assets.precompile = %w( imageloaded.pkgd.min.js )
+Rails.application.config.assets.precompile = %w( masonry.pkgd.min.js )
