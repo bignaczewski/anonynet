3177114e0a2ad65854e9d8824a3cb627669bd735
Bartosz Ignaczewski <bartosz.p.ignaczewski@gmail.com>
2015-07-23 15:06:03 +0200

    Heroku config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += [/.*\.js/, /.*\.css/]
+# Rails.application.config.assets.precompile += [/.*\.js/, /.*\.css/]
+Rails.application.config.assets.precompile += %w(modenizr.js imageloaded.pkgd.min.js masonry.pkgd.min.js)
