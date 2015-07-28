692254de93beac178e2a125241ea4aff3cceee06
Bartosz Ignaczewski <bartosz.p.ignaczewski@gmail.com>
2015-07-28 15:07:52 +0200

    mailer fix

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,7 @@ class Contact < MailForm::Base
   attribute :nickname, :name, :surname
   attribute :category, :validate => true
   attribute :message, :validate => true
-  attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i, :allow_blank => true
+  attribute :email, :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
